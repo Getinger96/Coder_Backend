490b8ebc3eec97de08f0b448f76a994aebeeb1ba
Getinger96 <erich.getinger@outlook.de>
2025-08-17 17:02:13 +0200

    first commit

---
+++ auth_app/api/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+
+urlpatterns = [
+    path('registration/', name='registration'),
+    path('login/',  name='login'),
+]
---
+++ auth_app/api/views.py
@@ -0,0 +1,9 @@
+from .serializers import  RegistrationSerializer
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework import status
+
+
