12cc132d2cc94d1f0ac29577aae35fadfd5367df
Getinger96 <erich.getinger@outlook.de>
2025-09-01 19:52:16 +0200

    added base-info view

--- coder/api/urls.py
+++ coder/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ProfileDetailView,ProfileCustomerView, ProfileBusinessView, OfferView,OfferDetailView,OfferDetailRetrieveView,OrderView,OrderDetailView,BusinessUserOrderCountView,BusinessUserOrderCompletedCountView,ReviewCreateView,ReviewDetailView
+from .views import ProfileDetailView,ProfileCustomerView, ProfileBusinessView, OfferView,OfferDetailView,OfferDetailRetrieveView,OrderView,OrderDetailView,BusinessUserOrderCountView,BusinessUserOrderCompletedCountView,ReviewCreateView,ReviewDetailView,BaseInfoView
 
 
 
@@ -17,6 +17,7 @@ urlpatterns = [
     path('completed-order-count/<int:business_user_id>/', BusinessUserOrderCompletedCountView.as_view(), name='order-completed-count'),
     path('reviews/', ReviewCreateView.as_view(), name='review-create'),
     path('reviews/<int:pk>/', ReviewDetailView.as_view(), name='review-detail'),
+    path('base-info/', BaseInfoView.as_view(), name='base-info'),
 
 
 
--- coder/api/views.py
+++ coder/api/views.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from django_filters.rest_framework import DjangoFilterBackend
-from django.db.models import Min
+from django.db.models import Min, Avg
 from rest_framework.pagination import PageNumberPagination
 from django.shortcuts import get_object_or_404
 from rest_framework.exceptions import PermissionDenied
@@ -254,3 +254,29 @@ class ReviewDetailView(generics.RetrieveUpdateDestroyAPIView):
         if review.reviewer.user != self.request.user:
             raise PermissionDenied("Du bist nicht berechtigt, diese Bewertung zu bearbeiten oder zu löschen.")
         return review
+    
+
+
+class BaseInfoView(APIView):
+    permission_classes = []  # Keine Auth erforderlich
+
+    def get(self, request):
+        try:
+            review_count = Review.objects.count()
+            average_rating = Review.objects.aggregate(avg_rating=Avg('rating'))['avg_rating'] or 0.0
+            business_profile_count = Profile.objects.filter(user__type='business').count()
+            offer_count = Offer.objects.count()
+
+            data = {
+                "review_count": review_count,
+                "average_rating": round(average_rating, 1),
+                "business_profile_count": business_profile_count,
+                "offer_count": offer_count
+            }
+            return Response(data, status=status.HTTP_200_OK)
+
+        except Exception as e:
+            return Response(
+                {"detail": "Ein interner Fehler ist aufgetreten."},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
